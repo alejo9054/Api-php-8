babe7cfe84b7cca4e04f4e74a16157af3706519b
Pedro Alejandro <alejo9054@gmail.com>
2021-01-28 18:56:42 -0500

    tablas principales de migracion  creadas

--- database/migrations/2021_01_19_015757_create_products_table.php
+++ database/migrations/2021_01_19_015757_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +16,18 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description',1000);#tiene un valor maximo de caracteres de 1000
+            $table->integer('quantity')->unsigned();
+            $table->string('status')->default(Product::PRODUCTO_NO_DISPONIBLE);
+            $table->string('image');
+            $table->integer('seller_id')->unsigned();
+            
             $table->timestamps();
+/**
+ * Aqui se le da referencia a la llave foranea
+ */
+            $table->foreign('seller_id')->references('id')->on('users');
         });
     }
 
--- database/migrations/2021_01_19_020107_create_transactions_table.php
+++ database/migrations/2021_01_19_020107_create_transactions_table.php
@@ -15,7 +15,19 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->integer('quantity')->unsigned();
+            $table->integer('buyer_id')->unsigned();
+            $table->integer('product_id')->unsigned();
             $table->timestamps();
+
+            /**
+             * Recordar que buyer_id y product_id son claves foraneas
+             * recordar que el parametro on recibe el nombre de la tabla y no del modelo(por eso esta en plural)
+             */
+            $table->foreign('buyer_id')->references('id')->on('users');
+            $table->foreign('product_id')->references('id')->on('products');
+            
+
         });
     }
 
