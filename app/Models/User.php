e0c8095146da9c84581e866342898afb922e26b9
Pedro Alejandro <alejo9054@gmail.com>
2021-02-11 11:45:42 -0500

    mutators to users created

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,6 +35,29 @@ class User extends Authenticatable
     ];
 
     /**
+     * Mutators to make sure that database is filled ok
+     */
+    //set all string names as  lower case  
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = strtolower($value);// we acces to the atributes list from this model and modify it
+    }
+    //get this atribute and show it better without modify it in data base
+    public function getNameAttribute($value)
+    {
+        return ucwords($value);
+    }
+    //set mutator to email
+    public function setEmailAttribute($value)
+    {
+        $this->attributes['email'] = strtolower($value);
+    }
+
+
+
+
+
+    /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
