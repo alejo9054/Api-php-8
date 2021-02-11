1d6de9565f5106478f7badc1ae7b245f722aa351
Pedro Alejandro <alejo9054@gmail.com>
2021-02-11 16:24:33 -0500

    post Error when  data were not sent and Json Error when an instance is not found in data base

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,16 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
+
+use App\Traits\ApiResponser;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
     /**
      * A list of the exception types that are not reported.
      *
@@ -37,4 +42,34 @@ class Handler extends ExceptionHandler
             //
         });
     }
+    
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ValidationException) {
+            return $this->convertValidationExceptionToResponse($exception, $request);
+        }
+        if ($exception instanceof ModelNotFoundException) {
+            $modelo = class_basename($exception->getModel());
+            return $this->errorResponse("No existe ninguna nnstancia de {$modelo} con el id espeficico", 404);
+        }
+        return parent::render($request, $exception);
+    }
+ 
+ 
+ 
+    /**
+     * Create a response object from the given validation exception.
+     *
+     * @param  \Illuminate\Validation\ValidationException  $e
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
+    {
+        $errors = $e->validator->errors()->getMessages();
+ 
+        return $this->errorResponse($errors, 422);
+    }
+ 
+
 }
