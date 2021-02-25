541e81748197b672c569ef3c8e76d6e2ad4779ca
Pedro Alejandro <alejo9054@gmail.com>
2021-02-25 16:46:09 -0500

    Seller Scope done

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -4,11 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\Product;
-
+use App\Scopes\SellerScope;
 
 class Seller extends User
 {
     use HasFactory;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new SellerScope);
+    }
     /**
      * un vendedor tiene muchos productos
      */
